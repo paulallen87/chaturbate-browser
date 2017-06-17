11ff9152c860585fcb31efc1818911faa3b2ed86
Paul Allen <paulallen87555@gmail.com>
2017-06-16 20:28:29 -0400

    updates publish script

--- publish.sh
+++ publish.sh
@@ -1,16 +1,29 @@
 #/bin/bash
 
 VERSION="$(node -p "require('./package.json').version")"
+CURRENT_VERSION="$(npm info . version)"
 COMMENT=
 
 git add . || exit 1
 git status
 
-echo "Comment (${VERSION}): "
+echo "Comment (${CURRENT_VERSION} -> ${VERSION}): "
 read COMMENT
 
 git commit -m "${COMMENT}" || exit 1
-git tag "v${VERSION}" || exit 1
+
+if [ "${VERSION}" != "${CURRENT_VERSION}" ]; then
+  echo "creating new git tag for ${VERSION}"
+  git tag "v${VERSION}" || exit 1
+else
+  echo "skipping git tag"
+fi
+
 git push origin master --tags || exit 1
 
-npm publish --access=public
+if [ "${VERSION}" != "${CURRENT_VERSION}" ]; then
+  echo "${CURRENT_VERSION} -> ${VERSION}"
+  npm publish --access=public
+else
+  echo "Versions are the same, not publishing"
+fi
