8df45e69e31c476e55ae38eb87f4c517eca6d248
Paul Allen <paulallen87555@gmail.com>
2017-06-16 20:07:47 -0400

    small updates

--- publish.sh
+++ publish.sh
@@ -3,14 +3,14 @@
 VERSION="$(npm info . version)"
 COMMENT=
 
-git add .
+git add . || exit 1
 git status
 
-echo 'Comment: '
+echo "Comment (${VERSION}): "
 read COMMENT
 
-git commit -m "${COMMENT}"
-git tag "v${VERSION}"
-git push origin master --tags
+git commit -m "${COMMENT}" || exit 1
+git tag "v${VERSION}" || exit 1
+git push origin master --tags || exit 1
 
 npm publish --access=public
